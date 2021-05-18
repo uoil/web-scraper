64bb97b09c32dbf924a1b9c92517755a4eece114
noil <robloxguy@protonmail.com>
2021-05-18 18:19:42 -0400

    added ebooks

--- index.js
+++ index.js
@@ -13,15 +13,27 @@ app.use(express.static(__dirname + '/public'));
 
 app.post('/scrapeChan', urlencodedParser, function (request, response) {
 	chanScraper.scrape(request.body.fullUrl);
-	console.log("Downloading 4chan thread " + request.body.fullUrl + " ...");
+	console.log("Downloading 4chan thread (" + request.body.fullUrl + ") ...");
 	response.redirect('/');
 	response.end();
 });
 
 app.post('/scrapeReddit', urlencodedParser, function (request, response) {
 	redditScraper.scrape(request.body.fullUrl);
-	console.log("Downloading subreddit " + request.body.fullUrl + " ...");
+	console.log("Downloading subreddit (" + request.body.fullUrl + ") ...");
 	response.redirect('/');
+	response.end();
+});
+
+app.post('/findBookLibgen', urlencodedParser, function (request, response) {
+	console.log("Finding book on libgen (" + request.body.bookTitle + ") ...");
+	response.redirect('//libgen.fun/search.php?req=' + request.body.bookTitle);
+	response.end();
+});
+
+app.post('/findBookZlibrary', urlencodedParser, function (request, response) {
+	console.log("Finding book on zlibrary (" + request.body.bookTitle + ") ...");
+	response.redirect('//1lib.us/s/' + request.body.bookTitle);
 	response.end();
 });
 
